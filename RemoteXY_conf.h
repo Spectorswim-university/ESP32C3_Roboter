a0c47d2d399f47a23a64b677cdf5c7cc9d0129f4
Volodymyr Tyshchenko <volodymyr@tyshchenko.de>
2025-12-29 09:48:39 +0100

    interface changed
    added speed controller and removed LED because element limit exceeded

--- RemoteXY_conf.h
+++ RemoteXY_conf.h
@@ -10,21 +10,20 @@
 #include <BLEDevice.h>
 
 // RemoteXY connection settings 
-#define REMOTEXY_BLUETOOTH_NAME "14-C"
-#define REMOTEXY_ACCESS_PASSWORD "14-C_password"
+#define REMOTEXY_BLUETOOTH_NAME "Roboter 14-C"
+#define REMOTEXY_ACCESS_PASSWORD "password_14-C"
 
 
 #include <RemoteXY.h>
 
 // RemoteXY GUI configuration  
 #pragma pack(push, 1)  
-uint8_t const PROGMEM RemoteXY_CONF_PROGMEM[] =   // 109 bytes V19 
-  { 255,4,0,1,0,102,0,19,0,0,0,82,111,98,111,116,101,114,32,49,
-  52,45,67,0,26,2,200,84,200,92,1,1,4,0,5,5,22,60,60,2,
-  24,66,66,32,16,26,31,3,152,1,45,82,149,1,50,90,2,62,26,70,
-  28,3,14,14,27,3,15,15,144,26,37,0,10,90,6,20,20,89,6,22,
-  22,48,4,26,24,83,101,110,115,111,114,115,32,79,78,0,31,83,111,110,
-  100,111,114,115,32,79,70,70,0 };
+uint8_t const PROGMEM RemoteXY_CONF_PROGMEM[] =   // 92 bytes V19 
+  { 255,5,0,0,0,85,0,19,0,0,0,82,111,98,111,116,101,114,32,49,
+  52,45,67,0,26,2,200,84,106,200,1,1,4,0,5,18,21,60,60,203,
+  50,143,143,32,119,26,31,10,106,5,15,15,20,7,57,57,48,4,26,31,
+  79,78,0,31,79,70,70,0,3,164,7,35,64,71,43,39,105,2,119,26,
+  4,3,43,12,36,58,95,7,86,0,119,28 };
   
 // this structure defines all the variables and events of your control interface 
 struct {
@@ -32,11 +31,9 @@ struct {
     // input variables
   int8_t angularSpeed; // from -100 to 100
   int8_t linearSpeed; // from -100 to 100
-  uint8_t position; // from 0 to 2
   uint8_t sensorsON; // =1 if state is ON, else =0, from 0 to 1
-
-    // output variables
-  uint8_t lineDetector; // from 0 to 1
+  uint8_t position; // from 0 to 2
+  int8_t gearBox; // from 0 to 100
 
     // other variable
   uint8_t connect_flag;  // =1 if wire connected, else =0
